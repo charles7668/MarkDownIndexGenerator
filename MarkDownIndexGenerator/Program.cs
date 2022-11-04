9a088fdc0fcbda9bc1538f35b60ebe3889504f5a
charles <charles76680119@gmail.com>
2022-11-04 11:30:42 +0800

    perf: remove unnecessary code

--- MarkDownIndexGenerator/Program.cs
+++ MarkDownIndexGenerator/Program.cs
@@ -14,10 +14,5 @@ static void Run(InputOption option)
     }
 
     var tree = MarkDownTree.GetMarkDownFileTree(Path.GetFullPath(option.RootFile));
-    // set root file
-    tree.Value = new FileInfo(option.RootFile);
-    var childs = tree.Childs;
-    if (childs != null)
-        tree.Childs = childs.Where(x => x.Value.FullName != tree.Value.FullName).ToList();
     tree.InsertIndexIntoTree(tree, option.TitleText);
 }
